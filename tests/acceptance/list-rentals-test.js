8ffe7b2d262cccc378bdae1426097f272776daa9
Ygam Retuta <ygam.j.retuta@gmail.com>
2018-02-14 11:25:47 +0800

    setup ember js files

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,42 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'mb-mtg/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should show rentals at home page', function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
+
+test('should link to information about company', function(assert){
+  visit('/');
+  click('a:contains("About Us")');
+  andThen(function(){
+    assert.equal(currentURL(), '/about', 'should navigate to about');
+  });
+});
+
+test('should link to contact information', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
+  });
+});
+
+test('should list available rentals', function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
+
+test('should filter the list of rentals by city', function(assert){
+
+});
+
+test('should show details for a selected rental', function(assert){
+
+});
